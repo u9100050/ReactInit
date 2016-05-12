3da0dcf0a57cdc682eb381c9848fefb0dd062e8e
NekrasovAA <u9100050@yandex.ru>
2016-05-12 12:39:27 +0300

    6

---
+++ src/components/SearchInput.js
@@ -0,0 +1,16 @@
+/**
+ * Created by NekrasovAA on 12.05.2016.
+ */
+import React, {PropTypes Component} from 'react';
+
+export default class SearchInput extends Component{
+  render(){
+      return(
+          <input type='search' className='filter'/>
+      )
+  }  
+};
+
+SearchInput.propTypes = {
+    
+};
